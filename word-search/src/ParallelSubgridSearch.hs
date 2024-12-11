328df25adfed7a84fc3d785e1c952a27d692a724
Sean Zhang <srz2116@columbia.edu>
2024-12-11 13:43:43 -0500

    split by subgrid bounds

--- word-search/src/ParallelSubgridSearch.hs
+++ word-search/src/ParallelSubgridSearch.hs
@@ -8,31 +8,28 @@ findWordsSubgrids :: Int -> [[Char]] -> [String] -> [String]
 findWordsSubgrids splits board wordsList =
     let subBoards = splitBoard splits board
         trie = foldr SequentialSearch.insertWord SequentialSearch.emptyTrie wordsList
-        results = parMap rdeepseq (\subBoard -> findWordsTrie subBoard trie) subBoards
+        results = parMap rdeepseq (\subBoard -> findWordsTrie board trie subBoard) subBoards
     in nub (concat results)
 
-findWordsTrie :: [[Char]] -> SequentialSearch.Trie -> [String]
-findWordsTrie board trie = nub $ concatMap (\(r,c) -> 
-    SequentialSearch.searchFromCell board trie r c []
-    ) [(r,c) | r <- [0..rows-1], c <- [0..cols-1]]
+findWordsTrie :: [[Char]] -> SequentialSearch.Trie -> (Int, Int, Int, Int)-> [String]
+findWordsTrie board trie (rStart, rEnd, cStart, cEnd) = 
+    nub $ concatMap (\(r,c) -> 
+        SequentialSearch.searchFromCell board trie r c []
+    ) [(r,c) | r <- [rStart..min rEnd (rows-1)], c <- [cStart..min cEnd (cols-1)]]
   where
     rows = length board
     cols = length (head board)
 
-splitBoard :: Int -> [[Char]] -> [[[Char]]]
+splitBoard :: Int -> [[Char]] -> [(Int, Int, Int, Int)]
 splitBoard n board
-    | n <= 1 = [board]
-    | n >= rows = [extractSubBoard board r (r + 1) c (c + 1) | r <- [0..rows-1], c <- [0..min rows cols-1]]
-    | n >= cols = [extractSubBoard board r (r + 1) c (c + 1) | r <- [0..min rows cols-1], c <- [0..cols-1]]
+    | n <= 1 = [(0, rows, 0, cols)]
+    | n >= rows = [(r, r + 1, c, c + 1) | r <- [0..rows-1], c <- [0..min rows cols-1]]
+    | n >= cols = [(r, r + 1, c, c + 1) | r <- [0..min rows cols-1], c <- [0..cols-1]]
     | otherwise =
-        [extractSubBoard board (r * subRows) ((r + 1) * subRows) (c * subCols) ((c + 1) * subCols)
+        [(r * subRows, (r + 1) * subRows, c * subCols, (c + 1) * subCols)
          | r <- [0..n-1], c <- [0..n-1]]
   where
     rows = length board
     cols = length (head board)
     subRows = rows `div` n
     subCols = cols `div` n 
-
-extractSubBoard :: [[Char]] -> Int -> Int -> Int -> Int -> [[Char]]
-extractSubBoard board rowStart rowEnd colStart colEnd =
-    [take (colEnd - colStart) . drop colStart $ row | row <- take (rowEnd - rowStart) . drop rowStart $ board]
