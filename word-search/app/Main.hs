409841cdb98c16e0a5be395be30c1a28ef228643
keithlo122 <keithlo122@gmail.com>
2024-12-12 15:16:32 -0500

    validation for parallel depth search

--- word-search/app/Main.hs
+++ word-search/app/Main.hs
@@ -53,7 +53,11 @@ runSolution solution board wordsList param =
         "parallelwords" -> ParallelWordsSearch.findWords board wordsList
         "paralleldepth" ->
             case param of
-                Just n -> ParallelDepthSearch.findWords n board wordsList
+                Just n -> 
+                    let maxWordLength = maximum (map length wordsList)
+                    in if n < maxWordLength
+                    then error $ "Input depth " ++ show n ++ " is too small. The largest word length is " ++ show maxWordLength ++ "."
+                    else ParallelDepthSearch.findWords n board wordsList
                 Nothing -> error "Missing depth argument for 'paralleldepth' solution."
         "parallelsubgrids" ->
             case param of
